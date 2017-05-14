4ccc3d1c33dabff4bc4f1a9ea4b46a4819fd987a
huisoido <584331125@qq.com>
2017-05-14 11:10:42 +0800

    IE中的事件对象

--- Chapter13/EventUtil.js
+++ Chapter13/EventUtil.js
@@ -9,6 +9,23 @@ var EventUtil = {
             element["on" + type] = handler;
         }
     },
+
+    getEvent: function(event) {
+        return event ? event : window.event;
+    },
+
+    getTarget: function(event) {
+        return event.target || event.srcElement;
+    },
+
+    preventDefault: function(event) {
+        if (event.preventDefault) {
+            event.preventDefault();
+        } else {
+            event.returnValue = false;
+        }
+    },
+
     removeHandler: function(element, type, handler) {
         if (element.removeEventListener) {
             element.removeEventListener(type, handler, false);
@@ -16,6 +33,14 @@ var EventUtil = {
             element.detachEvent("on" + type, handler);
         } else {
             element["on" + type] = null;
+        }
+    },
+
+    stopPropagation: function(event) {
+        if (event.stopPropagation) {
+            event.stopPropagation();
+        } else {
+            event.cancelBubble = true;
         }
     }
 }
