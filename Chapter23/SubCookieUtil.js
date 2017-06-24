5d1570b2e4540eeb58a818e95c877f0c47c4bbd7
huisoido <584331125@qq.com>
2017-06-24 19:27:24 +0800

    docs: 离线应用与客户端存储
    子Cookie

--- Chapter23/SubCookieUtil.js
+++ Chapter23/SubCookieUtil.js
@@ -38,4 +38,60 @@ var SubCookieUtil = {
         }
         return null;
     },
+
+    set: function(name, subName, value, expires, path, domain, secure){
+        var subcookies = this.getAll(name) || {};
+        subcookies[subName] = value;
+        this.setAll(name, subcookies, expires, path, domain, secure);
+    },
+
+    setAll: function(name, subcookies, expires, path, domain, secure){
+
+        var cookieText = encodeURIComponent(name) + "=",
+            subcookieParts = new Array(),
+            subName;
+
+        for (subName in subcookies) {
+            if (subName.length > 0 && subcookies.hasOwnProperty(subName)) {
+                subcookieParts.push(encodeURIComponent(subName) + "=" + encodeURIComponent(subcookies[subName]));
+            }
+        }
+
+        //书中这里印刷错误
+        if (subcookieParts.length > 0) {
+            cookieText += subcookieParts.join("&");
+
+            if (expires instanceof Date) {
+                cookieText += "; expires=" + expires.toGMTString();
+            }
+
+            if (path) {
+                cookieText += "; path=" + path;
+            }
+
+            if (domain) {
+                cookieText += "; domain=" + domain;
+            }
+
+            if (secure) {
+                cookieText += "; secure";
+            }
+        } else {
+            cookieText += "; expires=" + (new Date(0).toGMTString());
+        }
+
+        document.cookie = cookieText;
+    },
+
+    unset: function(name, subName, path, domain, secure){
+        var subcookies = this.getAll(name);
+        if (subcookies) {
+            delete subcookies[subName];
+            this.setAll(name, subcookies, null, path, domain, secure);
+        }
+    },
+
+    unsetAll: function(name, path, domain, secure){
+        this.getAll(name, null, new Date(0), path, domain, secure);
+    }
 }
